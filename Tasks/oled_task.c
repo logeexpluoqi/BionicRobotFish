6edcc55187e7769129031cc211d6c7d7542a66ea
luoqi <logeexpluoqi@outlook.com>
2021-01-15 13:49:17 +0800

    modified system display priority, reduced

--- Tasks/oled_task.c
+++ Tasks/oled_task.c
@@ -29,8 +29,8 @@ void sys_disp_init()
     EXTI_Init(&EXTI_InitStructure);
 
     NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 }
