fd7c5c364d50bff9f7ec847132f06d6c627787c6
luoqi <logeexpluoqi@outlook.com>
2021-01-22 19:39:27 +0800

    add ak_motor_data_cache

--- Control/control_info.h
+++ Control/control_info.h
@@ -8,5 +8,20 @@
 #ifndef _CONTRL_INFO_H
 #define _CONTRL_INFO_H
 
+typedef struct ak_motor_ctrl_cache
+{
+    unsigned char id;
+    
+    float p_dst;
+    float v_dst;
+    float t_dst;
+    float kp;
+    float kd;
+
+    float p_upload;
+    float v_upload;
+    float t_upload;
+} AkMotorCtrlInfoTypedef;
+
 
 #endif
