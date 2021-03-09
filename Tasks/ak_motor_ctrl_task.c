f5a0ea5071875cc194c55220d41a108e518c312f
luoqi <logeexpluoqi@outlook.com>
2021-03-09 17:30:51 +0800

    fix(ak..task): the last 2 motor feedback data err

--- Tasks/ak_motor_ctrl_task.c
+++ Tasks/ak_motor_ctrl_task.c
@@ -18,7 +18,7 @@ static uint8_t ak_motor_upload_cache[62] = {0};
 state_t ak_motor_ctrl_task()
 {
     uint8_t i, chr[2];
-    uint8_t ctrl_motor_num = 0;
+    uint8_t motor_num = 0;
     uint8_t mode;
     state_t state = OK;
 
@@ -31,32 +31,31 @@ state_t ak_motor_ctrl_task()
             if(akmotor[i].exist == 1)
             {
                 delay_us(330); // this delay is necessary for CAN bus transmmit
-                ak_motor_upload_cache[3 + i*2] = akmotor[i].id_dst;
-                ctrl_motor_num  = ctrl_motor_num + 1;
                 switch(mode)
                 {
                 case EN_MOTOR_MODE: 
                 {
-                    if(ak_motor_mode_set(akmotor[i].id_dst, EN_AK_MOTOR_MODE) == 0)
-                        ak_motor_upload_cache[4 + i*2] = 1;
+                    ak_motor_upload_cache[3 + motor_num*2] = akmotor[motor_num].id_dst;
+                    if(ak_motor_mode_set(akmotor[motor_num].id_dst, EN_AK_MOTOR_MODE) == 0)
+                        ak_motor_upload_cache[4 + motor_num*2] = 1;
                     else
-                        ak_motor_upload_cache[4 + i*2] = 0;
+                        ak_motor_upload_cache[4 + motor_num*2] = 0;
                     break;
                 }
                 case EX_MOTOR_MODE: 
                 {
-                    if(ak_motor_mode_set(akmotor[i].id_dst, EX_AK_MOTOR_MODE) == 0)
-                        ak_motor_upload_cache[4 + i*2] = 1;
+                    if(ak_motor_mode_set(akmotor[motor_num].id_dst, EX_AK_MOTOR_MODE) == 0)
+                        ak_motor_upload_cache[4 + motor_num*2] = 1;
                     else
-                        ak_motor_upload_cache[4 + i*2] = 0;
+                        ak_motor_upload_cache[4 + motor_num*2] = 0;
                     break;
                 }
                 case SET_MOTOR_ZERO: 
                 {
-                    if(ak_motor_mode_set(akmotor[i].id_dst, SET_AK_MOTOR_ZERO) == 0)
-                        ak_motor_upload_cache[4 + i*2] = 1;
+                    if(ak_motor_mode_set(akmotor[motor_num].id_dst, SET_AK_MOTOR_ZERO) == 0)
+                        ak_motor_upload_cache[4 + motor_num*2] = 1;
                     else
-                        ak_motor_upload_cache[4 + i*2] = 0;
+                        ak_motor_upload_cache[4 + motor_num*2] = 0;
                     break;
                 }
                 case CTRL_MOTOR: 
@@ -64,35 +63,36 @@ state_t ak_motor_ctrl_task()
                     AkMotorCtrlTypedef ak_motor_ctrl_data;
                     AkMotorInfo motor_feedback;
 
-                    ak_motor_ctrl_data.id    = akmotor[i].id_dst;
-                    ak_motor_ctrl_data.p_dst = akmotor[i].p_dst;
-                    ak_motor_ctrl_data.v_dst = akmotor[i].v_dst;
-                    ak_motor_ctrl_data.t_dst = akmotor[i].t_dst;
-                    ak_motor_ctrl_data.kp    = akmotor[i].kp;
-                    ak_motor_ctrl_data.kd    = akmotor[i].kd;
+                    ak_motor_ctrl_data.id    = akmotor[motor_num].id_dst;
+                    ak_motor_ctrl_data.p_dst = akmotor[motor_num].p_dst;
+                    ak_motor_ctrl_data.v_dst = akmotor[motor_num].v_dst;
+                    ak_motor_ctrl_data.t_dst = akmotor[motor_num].t_dst;
+                    ak_motor_ctrl_data.kp    = akmotor[motor_num].kp;
+                    ak_motor_ctrl_data.kd    = akmotor[motor_num].kd;
 
                     ak_motor_ctrl(&ak_motor_ctrl_data, &motor_feedback);
 
-                    akmotor[i].id_feedback = motor_feedback.id;
-                    akmotor[i].p_feedback  = motor_feedback.position;
-                    akmotor[i].v_feedback  = motor_feedback.velocity;
-                    akmotor[i].t_feedback  = motor_feedback.torque;
+                    akmotor[motor_num].id_feedback = motor_feedback.id;
+                    akmotor[motor_num].p_feedback  = motor_feedback.position;
+                    akmotor[motor_num].v_feedback  = motor_feedback.velocity;
+                    akmotor[motor_num].t_feedback  = motor_feedback.torque;
                     
-                    ak_motor_upload_cache[3 + i*7] = motor_feedback.id;
+                    ak_motor_upload_cache[3 + motor_num*7] = motor_feedback.id;
                     msg_float_to_char(motor_feedback.position, chr);
-                    ak_motor_upload_cache[4 + i*7] = chr[1];
-                    ak_motor_upload_cache[5 + i*7] = chr[0];
+                    ak_motor_upload_cache[4 + motor_num*7] = chr[1];
+                    ak_motor_upload_cache[5 + motor_num*7] = chr[0];
                     msg_float_to_char(motor_feedback.velocity, chr);
-                    ak_motor_upload_cache[6 + i*7] = chr[1];
-                    ak_motor_upload_cache[7 + i*7] = chr[0];
+                    ak_motor_upload_cache[6 + motor_num*7] = chr[1];
+                    ak_motor_upload_cache[7 + motor_num*7] = chr[0];
                     msg_float_to_char(motor_feedback.torque, chr);
-                    ak_motor_upload_cache[8 + i*7] = chr[1];
-                    ak_motor_upload_cache[9 + i*7] = chr[0];
+                    ak_motor_upload_cache[8 + motor_num*7] = chr[1];
+                    ak_motor_upload_cache[9 + motor_num*7] = chr[0];
                     break;
                 }
                 }
+                akmotor[motor_num].exist = 0;
+                motor_num  = motor_num + 1;
             }
-            akmotor[i].exist = 0;
         }
         msgbox_task_en(TASK_DISABLE);
 
@@ -102,17 +102,17 @@ state_t ak_motor_ctrl_task()
             {
                 ak_motor_upload_cache[0] = '{';
                 ak_motor_upload_cache[1] = mode;
-                ak_motor_upload_cache[2] = ctrl_motor_num * 2;
-                ak_motor_upload_cache[5 + ctrl_motor_num*2] = '}';
-                msg_put_computer(ak_motor_upload_cache, 6 + ctrl_motor_num*2);
+                ak_motor_upload_cache[2] = motor_num * 2;
+                ak_motor_upload_cache[5 + motor_num*2] = '}';
+                msg_put_computer(ak_motor_upload_cache, 6 + motor_num*2);
             }
             else if(mode == CTRL_MOTOR)
             {
                 ak_motor_upload_cache[0] = '{';
                 ak_motor_upload_cache[1] = mode;
-                ak_motor_upload_cache[2] = ctrl_motor_num * 7;
-                ak_motor_upload_cache[5 + ctrl_motor_num*7] = '}';
-                msg_put_computer(ak_motor_upload_cache, 6 + ctrl_motor_num*7);
+                ak_motor_upload_cache[2] = motor_num * 7;
+                ak_motor_upload_cache[5 + motor_num*7] = '}';
+                msg_put_computer(ak_motor_upload_cache, 6 + motor_num*7);
             }
         }
     }
